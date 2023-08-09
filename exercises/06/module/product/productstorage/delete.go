1c2dc77235216e63d3303c47e7589f1b49dd0e66
Luke <thth004d@spgroup.com.sg>
2023-08-09 13:51:38 +0700

    delete product

---
+++ exercises/06/module/product/productstorage/delete.go
@@ -0,0 +1,19 @@
+package productstorage
+
+import (
+	"context"
+
+	"github.com/hthai2201/dw-go-23/exercises/06/common"
+	"github.com/hthai2201/dw-go-23/exercises/06/module/product/productmodel"
+)
+
+func (s *productMySql) Delete(ctx context.Context, id int) error {
+	if err := s.db.Table(productmodel.Product{}.
+		TableName()).
+		Where("id = ?", id).
+		UpdateColumns(map[string]interface{}{"status": 0}).Error; err != nil {
+		return common.ErrDB(err)
+	}
+
+	return nil
+}
